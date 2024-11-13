321a3f6f7115f9ce9fad78a423915d053fd3952c
Felix <st157773@stud.uni-stuttgart.de>
2024-11-13 19:11:34 +0100

    step 5-7 + bonus (?)

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import versioneer
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -7,4 +8,6 @@ if __name__ == "__main__":
     setup(
         long_description=long_description,
         long_description_content_type="text/markdown",
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
     )
