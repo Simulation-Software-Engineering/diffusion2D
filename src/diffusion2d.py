d8875954626e8c401f05d0ba231e8ab8e7e773ce
Syed Mustafa Ahmed <syedmustafaahmad@gmail.com>
2024-11-15 14:33:06 +0100

    Added pyproject.toml, output.py, __init__.py. Modified diffusion2d.py, made solev() accept user arguements. Changed src file paths. All changes requirements for exercise to package the python code and upload to TestPyPI

--- src/diffusion2d.py
+++ src/diffusion2d.py
@@ -4,8 +4,10 @@ Solving the two-dimensional diffusion equation
 Example acquired from https://scipython.com/book/chapter-7-matplotlib/examples/the-two-dimensional-diffusion-equation/
 """
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from output import create_plot, output_plots
 
 # plate size, mm
 w = h = 10.
@@ -45,37 +47,54 @@ for i in range(nx):
 
 
 def do_timestep(u_nm1, u, D, dt, dx2, dy2):
-    # Propagate with forward-difference in time, central-difference in space
     u[1:-1, 1:-1] = u_nm1[1:-1, 1:-1] + D * dt * (
             (u_nm1[2:, 1:-1] - 2 * u_nm1[1:-1, 1:-1] + u_nm1[:-2, 1:-1]) / dx2
             + (u_nm1[1:-1, 2:] - 2 * u_nm1[1:-1, 1:-1] + u_nm1[1:-1, :-2]) / dy2)
-
     u_nm1 = u.copy()
     return u_nm1, u
 
+# Solver function
+def solve(dx=0.1, dy=0.1, D=4.0):
+    w = h = 10.0
+    T_cold = 300
+    T_hot = 700
+    nx, ny = int(w / dx), int(h / dy)
+    dx2, dy2 = dx * dx, dy * dy
+    dt = dx2 * dy2 / (2 * D * (dx2 + dy2))
+    u0 = T_cold * np.ones((nx, ny))
+    u = u0.copy()
+    r = min(h, w) / 4.0
+    cx = w / 2.0
+    cy = h / 2.0
+    r2 = r ** 2
+    for i in range(nx):
+        for j in range(ny):
+            p2 = (i * dx - cx) ** 2 + (j * dy - cy) ** 2
+            if p2 < r2:
+                u0[i, j] = T_hot
 
-# Number of timesteps
-nsteps = 101
-# Output 4 figures at these timesteps
-n_output = [0, 10, 50, 100]
-fig_counter = 0
-fig = plt.figure()
+    nsteps = 101
+    n_output = [0, 10, 50, 100]
+    fig = plt.figure()
+    fig_counter = 0
+    im = None
 
-# Time loop
-for n in range(nsteps):
-    u0, u = do_timestep(u0, u, D, dt, dx2, dy2)
+    for n in range(nsteps):
+        u0, u = do_timestep(u0, u, D, dt, dx2, dy2)
+        if n in n_output:
+            fig_counter += 1
+            im = create_plot(fig, u, T_cold, T_hot, dt, n, fig_counter)
 
-    # Create figure
-    if n in n_output:
-        fig_counter += 1
-        ax = fig.add_subplot(220 + fig_counter)
-        im = ax.imshow(u.copy(), cmap=plt.get_cmap('hot'), vmin=T_cold, vmax=T_hot)  # image for color bar axes
-        ax.set_axis_off()
-        ax.set_title('{:.1f} ms'.format(n * dt * 1000))
+    output_plots(fig, im)
 
-# Plot output figures
-fig.subplots_adjust(right=0.85)
-cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
-cbar_ax.set_xlabel('$T$ / K', labelpad=20)
-fig.colorbar(im, cax=cbar_ax)
-plt.show()
+# Running the solve function as a test
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python3 diffusion2d.py <dx> <dy> <D>")
+    else:
+        dx = float(sys.argv[2])
+        dy = float(sys.argv[3])
+        D = float(sys.argv[4])           
+        print(f'dx={dx}, dy={dy}, D={D}')
+
+        solve(dx, dy, D)
